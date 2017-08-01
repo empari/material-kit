86f8bbb953b9eae1d1ca18ff91e638f2f22f9be0
unknown <01.lexandre@gmail.com>
2017-08-01 19:21:00 -0300

    VSKEL-7 Created package empari-materialKit

---
+++ app/scripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
+  $.material.init()
+
+  //  Activate the Tooltips
+  $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip()
+})
