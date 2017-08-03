d2af9761e764aa0df333343e3706d5d0022e563d
Unknown <01.lexandre@gmail.com>
2017-08-02 22:08:34 -0300

    VSKEL-8 bootstrap-datepicker corrigido

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,11 @@
-$(document).ready(function () {
+window.$(document).ready(function () {
   // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
-  $.material.init()
+  window.$.material.init()
+
+  window.$('.datepicker').datepicker({
+    format: 'dd/mm/yyyy'
+  })
 
   //  Activate the Tooltips
-  $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip()
+  window.$('[data-toggle="tooltip"], [rel="tooltip"]').tooltip()
 })
